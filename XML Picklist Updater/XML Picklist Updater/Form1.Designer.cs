67fdac25be823064a0bbc09359b35fb3bd8f0460
D'Andrea Lopez, F. <f.dandrea.lopez@accenture.com>
2017-01-18 15:46:22 -0300

    Pendings identified: "UTF-8" in caps

--- XML Picklist Updater/XML Picklist Updater/Form1.Designer.cs
+++ XML Picklist Updater/XML Picklist Updater/Form1.Designer.cs
@@ -31,8 +31,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
             // 
             // label1
@@ -40,53 +41,59 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "No File Selected";
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Location = new System.Drawing.Point(12, 226);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(97, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load XMLs";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(197, 226);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(175, 226);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(97, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Create New XML";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(12, 25);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(260, 195);
-            this.listView1.TabIndex = 4;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.button2.Enabled = false;
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 25);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(260, 184);
+            this.checkedListBox1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "XML Picklist Updater";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,8 +104,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- XML Picklist Updater/XML Picklist Updater/Form1.cs
+++ XML Picklist Updater/XML Picklist Updater/Form1.cs
@@ -15,6 +15,9 @@ namespace XML_Picklist_Updater
 {
     public partial class Form1 : Form
     {
+        XmlDocument doc1 = new XmlDocument();
+        XmlDocument doc2 = new XmlDocument();
+        string filename;
         public Form1()
         {
             InitializeComponent();
@@ -22,25 +25,114 @@ namespace XML_Picklist_Updater
 
         private void button1_Click(object sender, EventArgs e)
         {
-            XmlDocument doc1 = new XmlDocument();
-            XmlDocument doc2 = new XmlDocument();
             DialogResult result1 = openFileDialog1.ShowDialog();
-            if (result1 == DialogResult.OK) 
+            if (result1 == DialogResult.OK)
             {
                 doc1.Load(openFileDialog1.FileName);
                 DialogResult result2 = openFileDialog1.ShowDialog();
                 if (result2 == DialogResult.OK)
                 {
+                    button2.Enabled = true;
+                    filename = openFileDialog1.SafeFileName;
+                    label1.Text = filename;
                     doc2.Load(openFileDialog1.FileName);
-                    XmlNodeList init = doc1.GetElementsByTagName("CustomObject");
-
+                    XmlNodeList iNode = doc1.GetElementsByTagName("CustomObject");
+                    XmlNodeList fNode = ((XmlElement)iNode[0]).GetElementsByTagName("fields");
+                    foreach (XmlElement fElem in fNode)
+                    {
+                        XmlNodeList eType = fElem.GetElementsByTagName("type");
+                        if (eType[0].InnerText == "Picklist" || eType[0].InnerText == "MultiselectPicklist")
+                        {
+                            XmlNodeList eFullName = fElem.GetElementsByTagName("fullName");
+                            checkedListBox1.Items.Add(eFullName[0].InnerText);
+                        }
+                    }
                 }
+                else
+                {
+                    button2.Enabled = false;
+                    label1.Text = "No File Selected";
+                    checkedListBox1.Items.Clear();
+                }
+            }
+            else
+            {
+                button2.Enabled = false;
+                label1.Text = "No File Selected";
+                checkedListBox1.Items.Clear();
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                XmlNodeList iNode = doc1.GetElementsByTagName("CustomObject");
+                XmlNodeList fNode = ((XmlElement)iNode[0]).GetElementsByTagName("fields");
+                foreach (XmlElement fElem in fNode)
+                {
+                    XmlNodeList eType = fElem.GetElementsByTagName("type");
+                    if (eType[0].InnerText == "Picklist" || eType[0].InnerText == "MultiselectPicklist")
+                    {
+                        XmlNodeList eFullName = fElem.GetElementsByTagName("fullName");
+                        if (checkedListBox1.CheckedItems.Contains(eFullName[0].InnerText))
+                        {
+                            XmlNodeList iiNode = doc2.GetElementsByTagName("CustomObject");
+                            XmlNodeList ffNode = ((XmlElement)iiNode[0]).GetElementsByTagName("fields");
+                            foreach (XmlElement ffElem in fNode)
+                            {
+                                XmlNodeList eeType = ffElem.GetElementsByTagName("type");
+                                if (eeType[0].InnerText == "Picklist" || eeType[0].InnerText == "MultiselectPicklist")
+                                {
+                                    XmlNodeList eeFullName = ffElem.GetElementsByTagName("fullName");
+                                    if (eFullName[0].InnerText == eeFullName[0].InnerText)
+                                    {
+                                        fElem.ReplaceChild(ffElem.GetElementsByTagName("picklist")[0], fElem.GetElementsByTagName("picklist")[0]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                XmlNodeList rNode = ((XmlElement)iNode[0]).GetElementsByTagName("recordTypes");
+                foreach (XmlElement rElem in rNode)
+                {
+                    XmlNodeList ePicklistValue = rElem.GetElementsByTagName("picklistValues");
+                    foreach(XmlElement pvElem in ePicklistValue)
+                    {
+                        if (checkedListBox1.CheckedItems.Contains(pvElem.GetElementsByTagName("picklist")[0].InnerText))
+                        {
+                            XmlNodeList iiNode = doc2.GetElementsByTagName("CustomObject");
+                            XmlNodeList rrNode = ((XmlElement)iiNode[0]).GetElementsByTagName("recordTypes");
+                            foreach (XmlElement rrElem in rrNode)
+                            {
+                                XmlNodeList eePicklistValue = rrElem.GetElementsByTagName("picklistValues");
+                                foreach (XmlElement ppvvElem in eePicklistValue)
+                                {
+                                    if (pvElem.GetElementsByTagName("picklist")[0].InnerText == ppvvElem.GetElementsByTagName("picklist")[0].InnerText)
+                                    {
+                                        doc1.ReplaceChild(ppvvElem, pvElem);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                XmlWriterSettings settings = new XmlWriterSettings
+                {
+                    Encoding = Encoding.UTF8,
+                    Indent = true,
+                    IndentChars = "    ",
+                    NewLineChars = "\r\n",
+                    NewLineHandling = NewLineHandling.Replace,
+                    CloseOutput = true
+                };
+                XmlWriter writer = null;
+                writer = XmlWriter.Create(folderBrowserDialog1.SelectedPath + "\\" + filename, settings);
+                doc1.Save(writer);
+                writer.Close();
+            }
         }
     }
 }
